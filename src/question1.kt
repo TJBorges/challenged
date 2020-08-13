2cb640ea73cbe5844b67c6614ad2658d98c0f550
TJ Borges <tj.borges3@gmail.com>
2020-08-12 23:47:56 -0300

    Primeiro dia

---
+++ src/question1.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var input: String = ""
+    var output: String = ""
+
+    //input = "primeira string de teste      "
+    input = "User is not allowed      "
+
+    output = input.trim().replace(" ", "&32").toString()
+
+    println(output)
+}
---
+++ src/question2.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    var input1: String = "moon"
+    var input2: String = "nmoo"
+
+    println(InputsIguais(input1, input2))
+}
+
+fun InputsIguais(inp1: String, inp2: String): String {
+
+    var lista1 = inp1.toCharArray()
+    var lista2 = inp2.toCharArray()
+    var falso: Int = 0
+
+
+    return if (inp1.substring(0, 2) == inp2.substring(0, 1)) {
+        "True"
+    } else {
+        if (inp1.length > 3) {
+            for (i in lista1.indices) {
+                if (lista1[i] != lista2[i]) {
+                    falso++
+                }
+            }
+        }
+        if (falso <= ((inp1.length / 3) * 2)) {
+            "True"
+        } else {
+            "False"
+        }
+    }
+}
