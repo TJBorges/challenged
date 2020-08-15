86c773674845706126c5a0dcd89e5132a932c852
TJ Borges <tj.borges3@gmail.com>
2020-08-15 00:55:27 -0300

    Quarto dia

---
+++ src/question5.kt
@@ -0,0 +1,20 @@
+import org.w3c.dom.Text
+import java.awt.TextArea
+
+fun main(args: Array<String>) {
+    
+
+    var emails = mutableMapOf<Email>()
+    emails.put(email1)
+
+    removerDuplicadas()
+
+}
+class Email(var topico: String, var conteudo: String) {
+}
+
+fun removerDuplicadas():Email {
+
+    return Email
+}
+
